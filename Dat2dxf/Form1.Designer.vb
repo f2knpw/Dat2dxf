599571c187b37f21e269eea64a34ebbfd741dea1
freedom2000 <31324055+f2knpw@users.noreply.github.com>
2026-02-16 22:37:39 +0100

    Ajoutez des fichiers projet.

---
+++ Dat2dxf/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿Imports System.Globalization
+Imports System.IO
+
+
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+    Private selectedFolderPath As String = ""
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        btnConvert = New Button()
+        lstLog = New ListBox()
+        picPreview = New PictureBox()
+        CType(picPreview, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' btnConvert
+        ' 
+        btnConvert.Location = New Point(657, 14)
+        btnConvert.Name = "btnConvert"
+        btnConvert.Size = New Size(168, 63)
+        btnConvert.TabIndex = 0
+        btnConvert.Text = "open .dat files directory"
+        btnConvert.UseVisualStyleBackColor = True
+        ' 
+        ' lstLog
+        ' 
+        lstLog.FormattingEnabled = True
+        lstLog.Location = New Point(856, 14)
+        lstLog.Name = "lstLog"
+        lstLog.Size = New Size(238, 424)
+        lstLog.TabIndex = 1
+        ' 
+        ' picPreview
+        ' 
+        picPreview.BackColor = SystemColors.ControlLightLight
+        picPreview.Location = New Point(14, 101)
+        picPreview.Name = "picPreview"
+        picPreview.Size = New Size(811, 329)
+        picPreview.TabIndex = 2
+        picPreview.TabStop = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1117, 450)
+        Controls.Add(picPreview)
+        Controls.Add(lstLog)
+        Controls.Add(btnConvert)
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        Name = "Form1"
+        Text = "dat to dxf"
+        CType(picPreview, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+
+    Friend WithEvents btnConvert As Button
+    Friend WithEvents lstLog As ListBox
+    Friend WithEvents picPreview As PictureBox
+
+End Class
---
+++ Dat2dxf/Form1.vb
@@ -0,0 +1,158 @@
+﻿Imports System.IO
+Imports System.Globalization
+Imports NetVector3 = netDxf.Vector3
+Imports netDxf
+Imports netDxf.Entities
+Imports System.Linq
+
+Public Class Form1
+    Private m_DossierCible As String = ""
+
+    ' --- BOUTON DE CONVERSION ET CHARGEMENT ---
+    Private Sub btnConvert_Click(sender As Object, e As EventArgs) Handles btnConvert.Click
+        ' 1. Effacer la liste immédiatement
+        lstLog.Items.Clear()
+
+        ' 2. Effacer l'aperçu précédent
+        If picPreview.Image IsNot Nothing Then
+            picPreview.Image.Dispose()
+            picPreview.Image = Nothing
+        End If
+
+        ' 3. Forcer la mise à jour visuelle avant d'ouvrir la boîte de dialogue
+        lstLog.Refresh()
+        picPreview.Refresh()
+
+        ' 4. Ouvrir la sélection de dossier
+        Dim fbd As New FolderBrowserDialog()
+        If fbd.ShowDialog() = DialogResult.OK Then
+            m_DossierCible = fbd.SelectedPath
+
+            ' Chargement et conversion
+            Dim files = Directory.EnumerateFiles(m_DossierCible, "*.*") _
+                        .Where(Function(s) s.ToLower.EndsWith(".dat") Or s.ToLower.EndsWith(".dxf"))
+
+            For Each filePath As String In files
+                If filePath.ToLower.EndsWith(".dat") Then
+                    Try
+                        ConvertirFichier(filePath)
+                    Catch : End Try
+                End If
+                lstLog.Items.Add(Path.GetFileName(filePath))
+            Next
+
+            ' Sélection automatique du premier profil
+            If lstLog.Items.Count > 0 Then lstLog.SelectedIndex = 0
+        End If
+    End Sub
+
+    ' --- LOGIQUE DE CONVERSION ---
+    Private Sub ConvertirFichier(fullPath As String)
+        Dim points As New List(Of NetVector3)()
+        Dim culture As CultureInfo = CultureInfo.InvariantCulture
+
+        For Each line As String In File.ReadLines(fullPath)
+            Dim parts = line.Split(New Char() {" "c, ControlChars.Tab}, StringSplitOptions.RemoveEmptyEntries)
+            Dim x, y As Double
+            If parts.Length >= 2 AndAlso Double.TryParse(parts(0), NumberStyles.Any, culture, x) AndAlso
+                                       Double.TryParse(parts(1), NumberStyles.Any, culture, y) Then
+                points.Add(New NetVector3(x * 100, y * 100, 0))
+            End If
+        Next
+
+        If points.Count > 2 Then
+            If Not (points.First().X = points.Last().X And points.First().Y = points.Last().Y) Then
+                points.Add(points.First())
+            End If
+            Dim doc As New DxfDocument()
+            doc.Entities.Add(New Spline(points))
+            doc.Save(Path.ChangeExtension(fullPath, ".dxf"))
+        End If
+    End Sub
+
+    ' --- GESTION DE L'AFFICHAGE ---
+    Private Sub lstLog_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstLog.SelectedIndexChanged
+        If lstLog.SelectedItem Is Nothing Then Exit Sub
+
+        Dim fileName As String = lstLog.SelectedItem.ToString()
+        Dim baseName As String = Path.GetFileNameWithoutExtension(fileName)
+        Dim extension As String = Path.GetExtension(fileName).ToLower()
+
+        Dim datPath As String = Path.Combine(m_DossierCible, baseName & ".dat")
+        Dim dxfPath As String = Path.Combine(m_DossierCible, baseName & ".dxf")
+
+        ' Logique de sélection :
+        If extension = ".dxf" Then
+            ' Si on clique sur le DXF, on affiche les deux pour comparer
+            DessinerAperçuDouble(datPath, dxfPath)
+        Else
+            ' Si on clique sur le DAT, on n'affiche que le bleu (DXF = "")
+            DessinerAperçuDouble(datPath, "")
+        End If
+    End Sub
+
+    Private Sub DessinerAperçuDouble(datPath As String, dxfPath As String)
+        Dim bmp As New Bitmap(picPreview.Width, picPreview.Height)
+        Using g As Graphics = Graphics.FromImage(bmp)
+            g.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+            g.Clear(Color.White)
+
+            Dim margin As Single = 20.0F
+            Dim scale As Single = CSng(picPreview.Width - (margin * 2))
+            Dim centerY As Single = CSng(picPreview.Height / 2)
+
+            ' Ligne de corde
+            g.DrawLine(Pens.LightGray, margin, centerY, CSng(picPreview.Width - margin), centerY)
+
+            ' 1. Dessin du DXF en ROUGE (Large)
+            If File.Exists(dxfPath) Then
+                Dim dxfPoints = LirePointsDxf(dxfPath)
+                If dxfPoints.Count > 1 Then
+                    Dim pts = dxfPoints.Select(Function(p) New PointF(margin + (p.X / 100.0F * scale), centerY - (p.Y / 100.0F * scale))).ToArray()
+                    g.DrawLines(New Pen(Color.Red, 3), pts)
+                End If
+            End If
+
+            ' 2. Dessin du DAT en BLEU (Fin)
+            If File.Exists(datPath) Then
+                Dim datPoints = LirePointsDat(datPath)
+                If datPoints.Count > 1 Then
+                    Dim pts = datPoints.Select(Function(p) New PointF(margin + (p.X * scale), centerY - (p.Y * scale))).ToArray()
+                    g.DrawLines(New Pen(Color.Blue, 1), pts)
+                End If
+            End If
+        End Using
+
+        If picPreview.Image IsNot Nothing Then picPreview.Image.Dispose()
+        picPreview.Image = bmp
+    End Sub
+
+    ' --- LECTURE DES POINTS ---
+    Private Function LirePointsDat(path As String) As List(Of PointF)
+        Dim pts As New List(Of PointF)()
+        Dim culture As CultureInfo = CultureInfo.InvariantCulture
+        For Each line In File.ReadAllLines(path)
+            Dim parts = line.Split(New Char() {" "c, ControlChars.Tab}, StringSplitOptions.RemoveEmptyEntries)
+            Dim x, y As Double
+            If parts.Length >= 2 AndAlso Double.TryParse(parts(0), NumberStyles.Any, culture, x) AndAlso
+                                       Double.TryParse(parts(1), NumberStyles.Any, culture, y) Then
+                pts.Add(New PointF(CSng(x), CSng(y)))
+            End If
+        Next
+        Return pts
+    End Function
+
+    Private Function LirePointsDxf(path As String) As List(Of PointF)
+        Dim pts As New List(Of PointF)()
+        Try
+            Dim doc = DxfDocument.Load(path)
+            ' On récupère les FitPoints de la première spline trouvée
+            If doc.Entities.Splines.Count > 0 Then
+                For Each v In doc.Entities.Splines.First().FitPoints
+                    pts.Add(New PointF(CSng(v.X), CSng(v.Y)))
+                Next
+            End If
+        Catch : End Try
+        Return pts
+    End Function
+End Class
